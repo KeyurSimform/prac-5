36ac3e8a229792d0eb4e445431f97c6aa75a7127
keyur <keyur.k@siformsolutions.com>
2022-03-28 20:05:04 +0530

    Committing the changes applied

--- src/componenet/UserDataContainer/UserDataContainer.jsx
+++ src/componenet/UserDataContainer/UserDataContainer.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import UserList from "../DummyData/DummyData";
 import User from "../User/User";
 import "../UserDataContainer/UserDataContainer.css";
 import UserDataHeader from "../UserDataHeader/UserDataHeader";
 import UserProfileCard from "../UserProfileCard/UserProfileCard";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import UserApi from "../UserApi/UserApi";
-import { userActions } from "../../store/UserSlice";
+import fetchAsyncUser  from "../../store/UserSlice";
 
 // Here the UserDataContainer is the componenet which hold all the main component of the app
 
@@ -22,37 +20,37 @@ const UserDataContainer = () => {
 	const dispatch = useDispatch();
 
 	useEffect(() => {
-		const fetchUser = async () => {
-			const response = await UserApi.get().catch((err) => {
-				console.log("Error: ", err);
-			});
-			console.log("Api response: ", response.data);
-			dispatch(userActions.addUser(response.data));
-		};
-		fetchUser();
-	}, []);
+
+		dispatch(fetchAsyncUser);
+		// const fetchUser = async () => {
+			// const response = await UserApi.get().catch((err) => {
+			// 	console.log("Error: ", err);
+			// });
+			// // console.log("Api response: ", response.data);
+			// dispatch(userActions.addUser(response.data));
+		// };
+		// fetchUser();
+	}, [dispatch]);
 
 	const UserCard = useSelector((state) => state.UserProfileCard.User);
 
-	const trial = useSelector((state)=> state.newUser.newUser);
+	const trial = useSelector((state) => state.newUser.newUser);
 
-	console.log(trial)
-	// const UserList = useSelector((state) => state.newUser.newUser);
-	// console.log(trial);
-	
+
 	return (
 		<div className="UserDataContainer">
 			<UserDataHeader />
-			{UserList.map((user) => (
-				<User
-					key={user.id}
-					id={user.id}
-					avatar={user.avatar}
-					fname={`${user.first_name}`}
-					lname={`${user.last_name}`}
-					email={user.email}
-				/>
-			))}
+			{trial.hasOwnProperty("data") &&
+				trial.data.map((user) => (
+					<User
+						key={user.id}
+						id={user.id}
+						avatar={user.avatar}
+						fname={`${user.first_name}`}
+						lname={`${user.last_name}`}
+						email={user.email}
+					/>
+				))}
 			{UserCard && (
 				<UserProfileCard
 					name={UserCard.name}
--- src/store/UserSlice.jsx
+++ src/store/UserSlice.jsx
@@ -1,4 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice , createAsyncThunk} from "@reduxjs/toolkit";
+import UserApi from "../componenet/UserApi/UserApi";
+
+export const fetchAsyncUser = createAsyncThunk('newUser/fetchAsyncUser', async () =>{
+	const response = await UserApi.get();
+	return response.data;
+})
+
 
 const userSlice = createSlice({
 	name: "newUser",
@@ -9,6 +16,18 @@ const userSlice = createSlice({
 		addUser(state, action) {
 			state.newUser = action.payload;
 		},
+	}, 
+	extraReducers: {
+		[fetchAsyncUser.pending]: ()=>{
+			console.log("pending")
+		},
+		[fetchAsyncUser.fulfilled]:(state , {payload})=>{
+			console.log("fullfiled")
+			return {...state, newUser:payload}
+		},
+		[fetchAsyncUser.rejected]:()=>{
+			console.log("Rejected");
+		}
 	}
 });
 export const userActions = userSlice.actions;
